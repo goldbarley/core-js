d851408d8f5fe73b4613a567a8c715afb860e734
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 05:44:03 +0700

    rename `es.string.at-alternative` -> `es.string.at`

--- tests/compat/tests.js
+++ tests/compat/tests.js
@@ -1254,7 +1254,7 @@ GLOBAL.tests = {
   }),
   'es.set.symmetric-difference.v2': createSetMethodTest('symmetricDifference'),
   'es.set.union.v2': createSetMethodTest('union'),
-  'es.string.at-alternative': function () {
+  'es.string.at': function () {
     return '𠮷'.at(-2) === '\uD842';
   },
   'es.string.code-point-at': function () {
