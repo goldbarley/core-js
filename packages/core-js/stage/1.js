5cbccf0055e1d5aba3af9052656fd924afa39662
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-06 14:39:04 +0700

    drop `String.prototype.codePoints` (proposal is dead)

--- packages/core-js/stage/1.js
+++ packages/core-js/stage/1.js
@@ -9,5 +9,4 @@ require('../proposals/data-view-get-set-uint8-clamped');
 require('../proposals/keys-composition');
 require('../proposals/math-clamp');
 require('../proposals/pattern-matching');
-require('../proposals/string-code-points');
 require('../proposals/string-cooked');
